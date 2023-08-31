663a059a11a9609b9bb72419777af7fe2e867db1
cyprux <justinprogproj@gmail.com>
2023-08-31 19:34:38 +0800

    Update Backend Syntax and PORT Numbers

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -13,11 +13,10 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(()=> console.log("MongoDB Connected..."))
-    .catch((err)=> console.log(err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
-    app.use("/api", routes);
-
+app.use("/api", routes);
 
 app.listen(PORT, () => console.log(`Listening at ${PORT}`));
